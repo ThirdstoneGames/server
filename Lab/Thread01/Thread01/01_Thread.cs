853dff567059fe7b1733d77860128ceed108c442
ThirdStomeGames <thirdstonegames@gmail.com>
2023-12-12 08:19:45 +0900

    Thread, Join, Threadpool, Task

---
+++ Lab/Thread01/Thread01/01_Thread.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+
+//namespace Thread01
+//{
+//    class Thread
+//    {
+//        static void MainThread()
+//        {
+//            for (int i = 0; i < 100; i++)
+//            {
+//                Console.WriteLine("Hello Thread!!!!" + i);
+//            }
+//        }
+//        static void Main(string[] args)
+//        {
+//            Thread t = new Thread(MainThread);
+//            t.Name = "Test Thread";
+//            t.IsBackground = true;
+//            t.Start();
+//        }
+//    }
+//}
---
+++ Lab/Thread01/Thread01/04_ThreadPool.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thread01
+{
+    internal class _04_ThreadPool
+    {
+        static void MainThread()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Main Thread!!");
+            }
+        }
+    }
+}
